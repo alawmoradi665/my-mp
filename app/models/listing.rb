3404a8519fb9d61daaa621b67307fe8b8aeb2505
alawmoradi665 <alaw.m@hotmail.com>
2021-08-07 11:31:26 +1000

    validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,10 @@
 class Listing < ApplicationRecord
     belongs_to :user
     has_many_attached :images
+    validates :price, presence: true
+    validates :title, presence: true
+    validates :images, presence: true
+    validates :state, presence: true
+    validates :city, presence: true
+    validates :suburb, presence: true
 end
