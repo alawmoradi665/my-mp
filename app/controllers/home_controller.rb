721b9f43adbf5cbcbada6dba5cd4e60513302d67
alawmoradi665 <alaw.m@hotmail.com>
2021-08-12 16:36:27 +1000

    jumbotron design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
   end
 
   def about
+    
   end
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,28 +1,31 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :authorize_listing, only: [:update, :edit, :destroy]
 
+  # GET /listings or /listings.json
   def index
     @listings = Listing.all
+    
   end
 
+  # GET /listings/1 or /listings/1.json
   def show
   end
 
+  # GET /listings/new
   def new
     @listing = Listing.new
     
   end
 
+  # GET /listings/1/edit
   def edit
   end
 
-  # Add current user_id to the listing
+  # POST /listings or /listings.json
   def create
     @listing = Listing.new(listing_params)
     @listing.user = current_user
-  
     if @listing.save
       redirect_to @listing, notice: 'Listing was successfully created.'
     else
@@ -30,14 +33,16 @@ class ListingsController < ApplicationController
     end
   end
 
+  # PATCH/PUT /listings/1 or /listings/1.json
   def update
-      if @listing.update(listing_params)
-        redirect_to @listing, notice: 'Listing was successfully updated.'
-      else
-        render :edit
-      end
+    if @listing.update(listing_params)
+      redirect_to @listing, notice: 'Listing was successfully updated.'
+    else
+      render :edit
+    end
   end
 
+  # DELETE /listings/1 or /listings/1.json
   def destroy
     @listing.destroy
     redirect_to listings_url, notice: 'Listing was successfully deleted.'
@@ -47,10 +52,11 @@ class ListingsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
       @listing = Listing.find(params[:id])
+
     end
 
     # Only allow a list of trusted parameters through.
     def listing_params
       params.require(:listing).permit(:title, :price, :description, :measurements, :state, :city, :suburb, images: [])
     end
-  end 
+end
